02fc765a9ace927b054ba595ec2229f551e95489
mmyl96 <niumiao960111@gmail.com>
2020-03-27 14:18:46 +0800

    Decrease learning rate from 0.01 to 0.0001, now it may converge

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class config:
     
     file_path = "./dataset/ratings.csv"
-    learning_rate = 0.01
-    epoch = 20000
+    learning_rate = 0.0001
+    epoch = 5000
     lamda = 0.0005
     nusers = 610
     nmovies = 193609
