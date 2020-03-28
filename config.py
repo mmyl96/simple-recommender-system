52ba19767733d5e3c7aca47a9c90ab886e9a47a6
mmyl96 <niumiao960111@gmail.com>
2020-03-28 15:18:27 +0800

    reduce training epoch

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class config:
     
     file_path = "./dataset/ratings.csv"
     learning_rate = 0.0001
-    epoch = 5000
+    epoch = 2000
     lamda = 0.0005
     nusers = 610
     nmovies = 193609
